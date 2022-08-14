9193a88c1df1b3b59a87069216f317309c1037b0
Maria <dudkomr97@gmail.com>
2022-08-14 17:36:14 +0300

    add button and button style

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// @ts-ignore
+import styles from "./App.module.css";
+
+const Button = ({ title, onClick, className, disabled }: any) => {
+  // const buttonType = {
+  //   primary: styles.primaty,
+  //   secondary: styles.secondary,
+  //   error: styles.error,
+  // }
+
+  return (
+    <button
+      onClick={onClick}
+      className={`${className || ""}`}
+      disabled={disabled}
+    >
+      {title}
+    </button>
+  );
+};
+const User = ({ name }: any) => {
+  return (<div>{name}</div>)
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.app}>
+     
+      <Button
+        title={"Primary"}
+        onClick={() => {
+          console.log("primary");
+        }}
+        className={styles.primary}
+        disabled={false}
+      />
+      <Button
+        title={"Primary"}
+        onClick={() => {
+          console.log("primary");
+        }}
+        className={styles.primary}
+        disabled={true}
+      />
+      <Button
+        title={"Secondary"}
+        onClick={() => {
+          console.log("secondary");
+        }}
+        className={styles.secondary}
+        disabled={false}
+      />
+      <Button
+        title={"Secondary"}
+        onClick={() => {
+          console.log("secondary");
+        }}
+        className={styles.secondary}
+        disabled={true}
+      />
+      <Button
+        title={"Error"}
+        onClick={() => {
+          console.log("error");
+        }}
+        className={styles.error}
+        disabled={false}
+      />
+      <Button
+        title={"Error"}
+        onClick={() => {
+          console.log("error");
+        }}
+        className={styles.error}
+        disabled={true}
+      />
+      
+      
+      
+        <User name={'Matt Helders'}/>
+      
+      
     </div>
   );
 }
