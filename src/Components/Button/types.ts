5e38ca4ff10dc217e2e702408bdee495aa569fd7
Maria <dudkomr97@gmail.com>
2022-08-21 15:47:41 +0300

    add components folder, with Button folder, Input folder

---
+++ src/Components/Button/types.ts
@@ -0,0 +1,16 @@
+export enum ButtonType {
+  Primary = "primary",
+  Secondary = "secondary",
+  Error = "error"
+}
+export type ButtonClassnamesType = {
+  [k in ButtonType]: string;
+};
+
+export type ButtonPropsType = {
+  title: string;
+  onClick: () => void;
+  className?: string;
+  disabled?: boolean;
+  type: ButtonType;
+};
---
+++ src/Components/Input/Input.tsx
@@ -0,0 +1,24 @@
+// import React, {FC,ChangeEvent} from 'react'
+// //@ts-ignore
+// import styles from './Input.module.css'
+// type InputProps = {
+//     value: string, 
+//     onChange: (value:string)=>void, 
+//     placeholder?: string, 
+//     disabled?: boolean, 
+//     error?: boolean,
+// }
+
+// const Input: FC<InputProps> = ({value, onChange, placeholder='', disabled, error}) => {
+   
+//    const onInputChange = (evt:ChangeEvent<HTMLInputElement>)=>{
+//     onChange(evt.target.value)
+//    }
+//     return(
+
+//         <input type="text" onChange={onInputChange} value={value} placeholder={placeholder} className={`${styles.input} ${ error ? styles.error : ''}`} disabled={disabled}/>
+//     )
+// }
+
+// export default Input
+export{}
