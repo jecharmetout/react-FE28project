41f72a62ab3fa72ea581660099f18e419613dbf6
Maria <dudkomr97@gmail.com>
2022-08-21 15:46:25 +0300

    add Navbar , move button to sepparate folder, add type to Button tag, add wrapper to components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import React, {useState} from "react";
+import React, { FC, useState } from "react";
 // @ts-ignore
 import styles from "./App.module.css";
-
-const Button = ({ title, onClick, className, disabled }: any) => {
-  // const buttonType = {
-  //   primary: styles.primaty,
-  //   secondary: styles.secondary,
-  //   error: styles.error,
-  // }
-
+import Button, {ButtonType} from './Components/Button'
+// import Menu from './components/navbar';
+// import Input from './components/Input';
+const Navbar = () => {
   return (
-    <button
-      onClick={onClick}
-      className={`${className || ""}`}
-      disabled={disabled}
-    >
-      {title}
-    </button>
+    <nav className={styles.wrapperNavbar}>
+      <div className={styles.burgerBtn}>
+        <span />
+      </div>
+      <main>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis
+          consequatur facere aut ullam, deleniti eligendi, blanditiis, vitae
+          maxime est sit atque in porro. Soluta eligendi dignissimos, iure nulla
+          ullam aliquid.
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis
+          consequatur facere aut ullam, deleniti eligendi, blanditiis, vitae
+          maxime est sit atque in porro. Soluta eligendi dignissimos, iure nulla
+          ullam aliquid.
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis
+          consequatur facere aut ullam, deleniti eligendi, blanditiis, vitae
+          maxime est sit atque in porro. Soluta eligendi dignissimos, iure nulla
+          ullam aliquid.
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis
+          consequatur facere aut ullam, deleniti eligendi, blanditiis, vitae
+          maxime est sit atque in porro. Soluta eligendi dignissimos, iure nulla
+          ullam aliquid.
+        </p>
+      </main>
+      {/* <Menu/> */}
+    </nav>
   );
 };
+
 
 const User = ({ userName }: any) => {
   return (
@@ -32,46 +54,94 @@ const Title = ({ title, className }: any) => {
     <div className={styles.wrapperTitle}>
       <h2 className={className}>{title}</h2>
     </div>
-  
-  )
+  );
 };
 const Tabs = () => {
   const tabItems = {
-    all: 'All',
-    myfav: 'My favorites',
-    popular: 'Popular',
-  }
-  const [value,setValue] = React.useState(0)
-  const actArray = []
-  for(let i=0;i<4;i++){
-    if (i===value){
-      actArray.push('btn active')
-    }
-    else{
-      actArray.push('btn')
+    all: "All",
+    myfav: "My favorites",
+    popular: "Popular"
+  };
+  const [value, setValue] = React.useState(0);
+  const actArray = [];
+  for (let i = 0; i < 4; i++) {
+    if (i === value) {
+      actArray.push("btn active");
+    } else {
+      actArray.push("btn");
     }
   }
   return (
     <>
-      
       <ul className={styles.tabList}>
-        <li><button type='button' className={actArray[0]} onClick={()=>{setValue(0)}}>All</button></li>
-        <li><button type='button' className={actArray[1]} onClick={()=>{setValue(1)}}>My favorites</button></li>
-        <li><button type='button' className={actArray[2]} disabled onClick={()=>{setValue(2)}}>Disabled</button></li>
-        <li><button type='button' className={actArray[3]} onClick={()=>{setValue(3)}}>Popular</button></li>
+        <li>
+          <button
+            type="button"
+            className={actArray[0]}
+            onClick={() => {
+              setValue(0);
+            }}
+          >
+            All
+          </button>
+        </li>
+        <li>
+          <button
+            type="button"
+            className={actArray[1]}
+            onClick={() => {
+              setValue(1);
+            }}
+          >
+            My favorites
+          </button>
+        </li>
+        <li>
+          <button
+            type="button"
+            className={actArray[2]}
+            disabled
+            onClick={() => {
+              setValue(2);
+            }}
+          >
+            Disabled
+          </button>
+        </li>
+        <li>
+          <button
+            type="button"
+            className={actArray[3]}
+            onClick={() => {
+              setValue(3);
+            }}
+          >
+            Popular
+          </button>
+        </li>
       </ul>
-      <br/><br/>
+      <br />
+      <br />
       <div>
-        <h2>TAB NO: {value+1}</h2>
+        <h2>TAB NO: {value + 1}</h2>
       </div>
     </>
   );
 };
 function App() {
+  // const [value, setValue] = useState<string>('');
+
+  // const onChange = (inputValue:string)=>{
+  //   setValue(inputValue)
+  // }
+
   return (
     <div className={styles.app}>
+      {/* <Input placeholder={'Placeholder'} onChange={onChange} value={value} disabled={true}/> */}
+      {/* <Navbar /> */}
       <div className={styles.wrapper}>
         <Button
+          type={ButtonType.Primary}
           title={"Primary"}
           onClick={() => {
             console.log("primary");
@@ -80,6 +150,7 @@ function App() {
           disabled={false}
         />
         <Button
+          type={ButtonType.Primary}
           title={"Primary"}
           onClick={() => {
             console.log("primary");
@@ -88,6 +159,7 @@ function App() {
           disabled={true}
         />
         <Button
+          type={ButtonType.Secondary}
           title={"Secondary"}
           onClick={() => {
             console.log("secondary");
@@ -96,6 +168,7 @@ function App() {
           disabled={false}
         />
         <Button
+          type={ButtonType.Secondary}
           title={"Secondary"}
           onClick={() => {
             console.log("secondary");
@@ -104,6 +177,7 @@ function App() {
           disabled={true}
         />
         <Button
+          type={ButtonType.Error}
           title={"Error"}
           onClick={() => {
             console.log("error");
@@ -112,6 +186,7 @@ function App() {
           disabled={false}
         />
         <Button
+          type={ButtonType.Error}
           title={"Error"}
           onClick={() => {
             console.log("error");
@@ -124,13 +199,12 @@ function App() {
       <div className={styles.wrapper}>
         <User userName={"Artem Malkin"} />
       </div>
-      <div className={`${styles.wrapper} ${styles.wrapperTabs}`} >
-        <Tabs/>
+      <div className={`${styles.wrapper} ${styles.wrapperTabs}`}>
+        <Tabs />
       </div>
       <div className={`${styles.wrapper}`}>
-        <Title title={'Sign In'} className={styles.title}/>
+        <Title title={"Sign In"} className={styles.title} />
       </div>
-      
     </div>
   );
 }
