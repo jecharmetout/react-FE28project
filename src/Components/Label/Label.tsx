30f40d369438a0b97f83f714c8751511ddf90dfb
Maria <dudkomr97@gmail.com>
2022-08-31 20:37:23 +0300

    add Label component

---
+++ src/Components/Label/Label.tsx
@@ -0,0 +1,12 @@
+import React, { FC, useState } from "react";
+//@ts-ignore
+import styles from "./Label.module.css";
+
+type LabelProps = {
+  title: string;
+};
+const Label: FC<LabelProps> = ({ title }) => {
+  return <div className={styles.label}>{title}</div>;
+};
+ 
+export default Label
