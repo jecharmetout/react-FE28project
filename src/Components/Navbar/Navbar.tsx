ef64c55116c5bc4337604c9066941c09d7b72471
Maria <dudkomr97@gmail.com>
2022-09-06 10:40:07 +0300

    add MoonIcon and SunIcon

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,29 +1,35 @@
-import React, { FC,useState } from "react";
+import React, { FC, useState } from "react";
 // import { HamburgerMenu } from "react-hamburger-menu";
 //@ts-ignore
 import styles from "./Navbar.module.css";
 import User from "../User/User";
-import {SearchIcon} from '../../Assets/Icons/index'
-import { MenuIcon } from "../../Assets/Icons";
-import { CancelIcon } from "../../Assets/Icons";
+import {
+  MenuIcon,
+  CancelIcon,
+  SearchIcon,
+  SunIcon,
+  MoonIcon
+} from "../../Assets/Icons";
+import classNames from "classnames";
 
-
-
-const Navbar = ({onClick,  input, isOpened}:any) => {
-
+const Navbar = ({ onClick, input, isOpened, isDark, changeThemeOnClick }: any) => {
   return (
     <nav className={styles.nav}>
-      <div
-        className={styles.burgerButton}
-        onClick={onClick}
-      >
-          {isOpened ? <CancelIcon /> : <MenuIcon />}
-
+      <div className={styles.burgerButton} onClick={onClick}>
+        {isOpened ? <CancelIcon /> : <MenuIcon />}
       </div>
       {input}
       <div className={styles.userSearchWrapper}>
-        <div className={styles.searchIcon} onClick={ ()=>{alert('Searh')}}>
-          <SearchIcon/>
+        <div className={classNames(styles.sunIcon)} onClick={changeThemeOnClick}>
+          {isDark ? <MoonIcon /> : <SunIcon />}
+        </div>
+        <div
+          className={styles.searchIcon}
+          onClick={() => {
+            alert("Searh");
+          }}
+        >
+          <SearchIcon />
         </div>
         <User userName={"Artem Malkin"} />
       </div>
